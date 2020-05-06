da63f49f04e528edfda80db79ba3849bc2325dec
bkchen <chenbk123>
2020-05-06 18:45:31 +0800

    commit and push

--- app/src/main/java/com/dyzs/ijkplayerdemo/activity/MainActivity.java
+++ app/src/main/java/com/dyzs/ijkplayerdemo/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.dyzs.ijkplayerdemo;
+package com.dyzs.ijkplayerdemo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dyzs.ijkplayerdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
+
     }
 }
